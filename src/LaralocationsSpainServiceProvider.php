8cc5a7251385b8bd38a46182c1a61a0616e2e1c2
Damián Aguilar <damian.aguilarm@gmail.com>
2025-06-27 12:04:05 +0200

    Update LaralocationsSpainServiceProvider.php

--- src/LaralocationsSpainServiceProvider.php
+++ src/LaralocationsSpainServiceProvider.php
@@ -18,12 +18,12 @@ class LaralocationsSpainServiceProvider extends ServiceProvider
         // 1. Publicar migraciones
         $this->publishes([
             __DIR__.'/../database/migrations' => database_path('migrations'),
-        ], 'laralocations-spain-migrations');
+        ], 'laralocations-spain');
 
         // 2. Publicar seeders
         $this->publishes([
             __DIR__.'/../database/seeders' => database_path('seeders'),
-        ], 'laralocations-spain-seeders');
+        ], 'laralocations-spain');
 
         // 3. Publicar archivos de datos
         $this->publishes([
